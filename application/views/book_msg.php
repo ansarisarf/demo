3f37080b86ce731cd30527d64485bf0cc8ba2745
ansarisarf <ansari.sarfraz@siesgst.ac.in>
2019-11-24 21:09:23 +0530

    initial code

---
+++ application/views/book_msg.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FirstView</title>
+</head>
+<body>
+<h1>First View MVC</h1>
+<p>Hello World.............</p>
+<hr>
+</body>
+</html>
---
+++ application/views/footer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<footer>
+  <h1>Footer</h1>
+  <p>Posted by: Hege Refsnes</p>
+  <p>Contact information: <a href="mailto:ansari.sarfraz@example.com">ansari.sarfraz@example.com</a>.</p>
+</footer>
+
+<p><strong>Note:</strong> The footer tag is not supported in Internet Explorer 8 and earlier versions.</p>
+<p>Copy right reserved &copy; 2019</p>
+
+</body>
+</html>
